fbaccfe33667028884e24cee08e34ac6c4168ebe
shaluhari <115150452+shaluhari@users.noreply.github.com>
2022-10-06 16:09:22 +0530

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<div align="center">
+<table border="0">
+<form action="ex22.php" method="post">
+<tr><td>name:</td><td><input type="text" name="a"></td></tr>
+<tr><td>rollno:</td><td><input type="text" name="b"></td></tr>
+<tr><td>depart:</td><td><input type="text" name="c"></td></tr>
+<tr><td>year:</td><td><input type="text" name="d"></td></tr>
+<tr><td><input type="submit"></td>
+</tr>
+</form>
+</table>
+</div>
+</body>
+</html> 
